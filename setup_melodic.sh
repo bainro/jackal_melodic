6d619cb8bb974dc3b5fd7fcc6c09a343f854274b
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-09-19 21:26:33 -0700

    Update setup_melodic.sh

--- setup_melodic.sh
+++ setup_melodic.sh
@@ -1,4 +1,8 @@
 #/bin/bash
+if [ $(lsb_release -sc) != "focal" ]; then 
+	echo "Error: Ubuntu 18.04 is required for ROS melodic."
+	exit
+fi
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt install curl 
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
