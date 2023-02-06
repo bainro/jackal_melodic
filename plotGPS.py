49c0e876af652aa59b3eef95f82c4909ec0d7a34
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-02-06 10:29:13 -0800

    Update plotGPS.py

--- plotGPS.py
+++ plotGPS.py
@@ -3,4 +3,5 @@ import csv
 with open('2_2_23_gps.csv', mode ='r') as f:
   csv_f = csv.reader(f)
   for line in csv_f:
-        print(line, "\n")
+        lat, long = line[1:3]
+        print(lat,long)
