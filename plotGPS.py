476219db978da3439a9ab8ce5d87a24877340a1e
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-02-06 10:57:33 -0800

    Update plotGPS.py

--- plotGPS.py
+++ plotGPS.py
@@ -24,8 +24,8 @@ with open('2_2_23_gps.csv', mode ='r') as f:
           
 print("max: ", max(dx_between_pts), " min: ", min(dx_between_pts)) 
 plt.plot(dx_between_pts)
-dx_sorted = dx_between_pts.sort()
-plt.plot(dx_sorted)
+#dx_sorted = dx_between_pts.sort()
+#plt.plot(dx_sorted)
 plt.show()
 
 '''
