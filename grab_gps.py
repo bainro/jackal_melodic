40e276063cd9ef15edd1ce134aac1cbec66d7183
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-08-26 03:00:48 -0700

    Create grab_gps.py

---
+++ grab_gps.py
@@ -0,0 +1,32 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+    def do_GET(self):
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        lat_long = post_data.decode("utf-8").split("&")
+        lat, long = lat_long[0], lat_long[1]
+        print(f'lat: {lat}')
+        print(f'long: {long}')
+        self._set_response()
+
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(httpd)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+
+if __name__ == '__main__':
+    from sys import argv
+    run()
