ae4bb92372d40860d31ef917349cd5270753dcec
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-09-13 14:10:28 -0400

    Create repub_calib_imu.py

---
+++ repub_calib_imu.py
@@ -0,0 +1,24 @@
+# apply calibration results to imu's magnetometer & publish in ROS
+import os
+import sys
+import rospy
+from sensor_msgs.msg import MagneticField
+
+mag_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+def repub_mag(msg):
+  
+  mag_pub.publish(msg)
+
+if __name__ == '__main__':
+    assert len(sys.argv) == 2, "pass in name of the mag topic" 
+    mag_topic = str(sys.argv[-1])
+    .lidarnode = rospy.Subscriber(mag_topic, MagneticField, repub_mag)
+    rospy.init_node("calib_mag")
+    rate = rospy.Rate(15) # Hz
+    pub = rospy.Publisher("/wifi_strength", Int32, queue_size=10)
+  
+    while not rospy.is_shutdown():
+        msg = Int32()
+        msg.data = wifi_sig
+        pub.publish(msg)
+        rate.sleep()
