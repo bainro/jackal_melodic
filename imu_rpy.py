422006c287fd11f02b73067b50636a9af1fb9e25
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-09-21 13:50:09 -0700

    Update and rename repub_calib_imu.py to imu_rpy.py

---
+++ imu_rpy.py
@@ -0,0 +1,24 @@
+# get roll, pitch, and yaw from imu sensor
+import sys
+import rospy
+from sensor_msgs.msg import Imu
+
+def get_rpy(m):
+  x = m.orientation.x
+  y = m.orientation.y
+  z = m.orientation.z
+  w = m.orientation.w
+  roll, pitch, _yaw = tf.transformations.euler_from_quaternion([x, y, z, w])
+  print(f'roll: {roll}')
+  print(f'pitch: {pitch}')
+
+if __name__ == '__main__':
+    assert len(sys.argv) == 2, "pass in name of the mag topic" 
+    rospy.init_node("calib_mag")
+    imu_topic = str(sys.argv[-1])
+    lidarnode = rospy.Subscriber(imu_topic, Imu, get_rpy)
+    
+    rate = rospy.Rate(4) # Hz
+  
+    while not rospy.is_shutdown():
+        rate.sleep()
