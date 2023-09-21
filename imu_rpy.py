f358f4c549dd144db57238489b7334554a14c16f
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-09-21 17:27:59 -0400

    Update imu_rpy.py

--- imu_rpy.py
+++ imu_rpy.py
@@ -12,6 +12,12 @@ def get_rpy(m):
   roll, pitch, _yaw = tf.transformations.euler_from_quaternion([x, y, z, w])
   print(f'roll: {roll}')
   print(f'pitch: {pitch}')
+  if roll < 0:
+    roll += 3.14
+  else:
+    roll -= 3.14
+  slope_cost = abs(roll) + abs(pitch)
+  print(f'slope cost: {slope_cost}')
 
 if __name__ == '__main__':
     assert len(sys.argv) == 2, "pass in name of the mag topic" 
