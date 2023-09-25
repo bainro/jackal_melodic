1aea24e86f1dcd5baa321c36c3b8167da443b821
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-09-25 01:02:49 -0700

    Update viz_gps_imu.py

--- viz_gps_imu.py
+++ viz_gps_imu.py
@@ -11,7 +11,7 @@ parser.add_argument("--bag_file", type=str, required=True, help=_help)
 args = parser.parse_args()
 
 if __name__ == "__main__":
-  prev_lat, prev_long = None, None
+  prev_lat, prev_lon = None, None
   diff_poses = []
   bag = rosbag.Bag(args.bag_file)
   for topic, msg, t in bag.read_messages(topics='/navsat/fix'):
@@ -21,11 +21,11 @@ if __name__ == "__main__":
     if prev_lat == None:
       print("only expecting this msg once @ beginning of bag")
     else:
-      diff_lats = abs(curr_lat  - prev_lat)
-      diff_longs = abs(curr_long - prev_long)
+      diff_lats = abs(curr_lat - prev_lat)
+      diff_lons = abs(curr_lon - prev_lon)
       # scary triangle maths
-      diff_poses.append((diff_lat ** 2 + diff_long ** 2) ** 0.5)
-    prev_lat  = curr_lat
-    prev_long = curr_long  
+      diff_poses.append((diff_lat ** 2 + diff_lon ** 2) ** 0.5)
+    prev_lat = curr_lat
+    prev_lon = curr_long
   bag.close()
   plt.hist(diff_poses, bins=10)
